4cf69e5fd3e53321eba7815157b0d96e058e5fda
Arpenaboyina Rakesh <rakesharpenaboyina2004@gmail.com>
2025-03-30 14:57:32 +0530

    new changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,31 +1,31 @@
-const { Sequelize, DataTypes } = require('sequelize');  
+const { Sequelize } = require('sequelize');  
 
 // Initialize Sequelize connection  
 const sequelize = new Sequelize('lorezone', 'admin', 'Rakesh281004', {  
     host: 'lorezone-database1.cl8qoikeqdft.ap-south-1.rds.amazonaws.com',  
-    dialect: 'mysql', // Specify the dialect (in this case, MySQL)  
-    operatorsAliases: false,  
-    pool: {  
-        max: 5,  
-        min: 0,  
-        acquire: 30000,  
-        idle: 10000  
-    }  
+    dialect: 'mysql',  
+    logging: false // Disable SQL query logging if you don't need it  
 });  
 
-const db = {};  
-db.Sequelize = sequelize;  
-db.models = {};  
-db.models.User = require('./user')(sequelize, Sequelize.DataTypes);  
-module.exports = db;  
+// Test the connection  
+async function testConnection() {  
+    try {  
+        await sequelize.authenticate();  
+        console.log('Connection has been established successfully.');  
+    } catch (error) {  
+        console.error('Unable to connect to the database:', error);  
+    }  
+}  
 
-// Close Sequelize connection when the Node.js process exits  
-process.on('exit', () => {  
-    sequelize.close()  
-        .then(() => {  
-            console.log('Sequelize connection closed');  
-        })  
-        .catch((error) => {  
-            console.error('Error closing Sequelize connection:', error);  
-        });  
+// Execute the connection test  
+testConnection();  
+
+// Close the connection when the Node.js process exits  
+process.on('exit', async () => {  
+    try {  
+        await sequelize.close();  
+        console.log('Sequelize connection closed');  
+    } catch (error) {  
+        console.error('Error closing Sequelize connection:', error);  
+    }  
 });  
