21a02b533edba2521de22e7a9fa0b2df66149272
Arpenaboyina Rakesh <rakesharpenaboyina2004@gmail.com>
2025-04-11 00:20:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+// import About from "./pages/About";
+import Login from "./pages/Login";
+import DreamBig from "./pages/DreamBig";
+import WatchDropStyle from "./pages/WatchDropStyle";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Trends from "./pages/Trends";
+import Events from "./pages/Events"; 
+import WatchMe from "./pages/WatchMe"; 
+import Brandsnearyou from "./pages/Brandsnearyou";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/dream-big" element={<DreamBig />} />
+        <Route path="/watchdropstyle" element={<WatchDropStyle />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/registerpage" element={<RegisterPage />} />
+        <Route path="/trends" element={<Trends />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/watchme" element={<WatchMe />} />
+        <Route path="/Brandsnearyou" element={<Brandsnearyou />} />
+      </Routes>
+    </Router>
   );
 }
 
